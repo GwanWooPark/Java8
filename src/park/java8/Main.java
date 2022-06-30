0d825679109174e64c84db592735fa7d397e99cc
GwanWooPark <pgw4712@icloud.com>
2022-06-30 21:09:54 +0900

    Functional Interface

---
+++ src/park/java8/Main.java
@@ -0,0 +1,22 @@
+package park.java8;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        // 익명 내부 클래스 (anonymous inner class)
+//        RunSomething runSomething = new RunSomething() {
+//            @Override
+//            public void doIt() {
+//                System.out.println("Hello, Java8");
+//            }
+//        };
+
+        // Lambda Expression
+        // 2줄이면 {} 안에 작성해야함
+        RunSomething runSomething = number -> {
+            return number + 10;
+        };
+
+        System.out.println(runSomething.doIt(20));
+    }
+}
---
+++ src/park/java8/RunSomething.java
@@ -0,0 +1,17 @@
+package park.java8;
+
+// 추상 메서드가 하나만 있으면 함수형 인터페이스
+@FunctionalInterface
+public interface RunSomething {
+
+    int doIt(int number);
+
+    static void printName() {
+        System.out.println("Park");
+    }
+
+    default void printAge() {
+        System.out.println("28");
+    }
+
+}
